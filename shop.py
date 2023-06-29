ecb72127d665311b08b2c809585f7fbb231e4423
Danielle Parker-Jones <dpjones01@hotmail.com>
2023-06-29 14:52:45 +0100

    Add new files

--- shop.py
+++ shop.py
@@ -4,62 +4,68 @@ class ThreeFailedAttempts(Exception):
 class InsufficientFundsError(Exception):
     pass
 
-prices = {"Wardrobe": 75, "Rug": 20, "Art": 1000}
-balance = 100
-purchase_attempts = 0
+def check_input(user_input, prices):
+    if user_input not in prices:
+        raise ValueError("Input not found in dictionary")
 
-print("Welcome")
+def check_affordability(user_input, prices, balance, purchase_attempts):
+    if prices[user_input] > balance:
+        if purchase_attempts >= 3:
+            raise ThreeFailedAttempts("You have had three failed attempts.")
 
-while True:
-    print("The items and their prices:")
-    for key, value in prices.items():
-        print(f"{key}: £{value}")
-    print("Please type exit to leave the shop")
+def check_extra_money(more_money, user_input, prices):
+    more_money = float(more_money)
+    if more_money <= 0:
+        raise ValueError("Invalid entry, must be more than 0")
 
-    user_input = input("Enter your choice: ")
+    if prices[user_input] > more_money:
+        raise InsufficientFundsError("Insufficient funds even with extra money")
+    return more_money
 
-    if user_input.lower() == "exit":
-        print("Goodbye")
-        break
+def shop():
+    prices = {"Wardrobe": 75, "Rug": 20, "Art": 1000}
+    balance = 100
+    purchase_attempts = 0
+    purchased_items = []
 
-    try:
-        if user_input not in prices:
-            raise ValueError("Input not found in dictionary")
+    print("Welcome")
 
-        if prices[user_input] > balance:
+    while True:
+        print("The items and their prices:")
+        for key, value in prices.items():
+            print(f"{key}: £{value}")
+        print("Please type exit to leave the shop")
+
+        user_input = input("Enter your choice: ")
+
+        if user_input.lower() == "exit":
+            print("Goodbye")
+            break
+
+        try:
+            check_input(user_input, prices)
+            check_affordability(user_input, prices, balance, purchase_attempts)
+
             print("Sorry, you cannot afford this item")
-
-            if purchase_attempts >= 3:
-                raise ThreeFailedAttempts("You have had three failed attempts.")
 
             more_money = input("Do you have more money? If so enter the amount, if not type exit: ")
             if more_money.lower() == "exit":
                 print("Goodbye")
                 break
 
-            try:
-                more_money = float(more_money)
-                if more_money <=0:
-                    raise ValueError("Invalid entry, must be more than 0")
+            more_money = check_extra_money(more_money, user_input, prices)
 
-                balance += more_money
-                purchase_attempts += 1
+            balance -= prices[user_input]
+            purchased_items.append(user_input)
+            print(f"Here's your {user_input}!")
 
-                if prices[user_input] > more_money:
-                    raise InsufficientFundsError("Insufficient funds even with extra money")
+        except ValueError as e:
+            print(f"Error: {e}")
 
-            except ValueError:
-                raise ValueError("This is an invalid amount")
+        except ThreeFailedAttempts as e:
+            print(f"Error: {e}")
+            print("Sorry you have had the maximum three attempts")
+            break
 
-            else:
-                balance -= prices[user_input]
-                print(f"Here's your {user_input}!")
-
-    except ValueError as e:
-        print(f"Error: {e}")
-    except ThreeFailedAttempts as e:
-        print(f"Error {e}")
-        print("Sorry you have had the maximum three attempts")
-        break
-
-
+if __name__ == "__main__":
+    shop()
